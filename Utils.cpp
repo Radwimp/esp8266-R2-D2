0156ffb32043e6339f35483617bd06308c73a3c6
Vasiliy <geass23621@gmail.com>
2020-03-11 14:00:32 +0200

    Feature: add jsonFormatter fn

--- Utils.cpp
+++ Utils.cpp
@@ -2,6 +2,16 @@
 #include "Utils.h"
 #include "ArduinoJson.h"
 
-String Utils::jsonFormatter() {
+String Utils::jsonFormatter(int size, int argsAmount, unsigned first, ...) {
+  String json;
+  DynamicJsonDocument doc(size);
+  JsonArray data = doc.createNestedArray("data");
+  unsigned *p = &first;
+
+  while(argsAmount--) {
+    data.add(*p);
+  }
   
+  serializeJson(doc, json);
+  return json;
 }
